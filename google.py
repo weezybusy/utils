5bcf87d4fb7e1c7001ae8aa4593d338fca9731cc
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2016-08-10 13:32:41 +0300

    initial commit

---
+++ google.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# google.py -- Opens google page with search results for provide keywords.
+
+import sys
+import webbrowser
+
+if len(sys.argv) < 2:
+    print('Next time add some keyword, stupid.')
+    sys.exit(0)
+
+google = 'https://www.google.com.ua/search?q='
+it = '+'.join(sys.argv[1:])
+webbrowser.open(google + it)
