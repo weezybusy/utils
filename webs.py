6f709eb3968a3cd985fbc0b86a28043efd7cf02c
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2016-08-11 11:25:11 +0300

    update

---
+++ webs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# webs.py - Opens tabs with web pages I use constantly.
+
+import webbrowser
+
+pages = [ 'https://github.com/weezybusy',
+          'https://keep.google.com',
+          'https://translate.google.com.ua',
+          'https://mail.google.com/mail/u/0/#inbox',
+          'https://www.youtube.com',
+          'https://www.khanacademy.org',
+          'http://clc-wiki.net/wiki/K%26R2_solutions' ]
+
+for page in pages:
+    webbrowser.open(page)
