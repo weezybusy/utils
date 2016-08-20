7fdab9029895011f1f4d430fb3f6b306b64f48db
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2016-08-20 14:43:10 +0300

    update translate.py

--- translate.py
+++ translate.py
@@ -4,22 +4,21 @@
 import urllib.request
 import sys
 
-agent = {'User-Agent':"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"}
-
-def translate(to_translate, to_langage="auto", langage="auto"):
+def translate(string, to_lang="auto", from_lang="auto"):
     before_trans = 'class="t0">'
-    link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s" % (to_langage, langage, to_translate.replace(" ", "+"))
-    request = urllib.request.Request(link, headers=agent)
+    url = "http://translate.google.com/m?hl={}&sl={}&q={}".format(to_lang, from_lang, string)
+    agent = { 'User-Agent': 'Mozilla/5.0', 'Accept-Charset': 'utf-8' }
+    request = urllib.request.Request(url, headers=agent)
     page = urllib.request.urlopen(request).read().decode("utf-8")
     result = page[page.find(before_trans)+len(before_trans):]
     result = result.split("<")[0]
-    return(result)
+    return result
 
 def main():
     if len(sys.argv) < 2:
         print('Nothing to translate.')
         sys.exit(0)
-    string = ' '.join(sys.argv[1:])
+    string = '+'.join(sys.argv[1:])
     result = translate(string, "ru")
     print('> ' + result)
 
